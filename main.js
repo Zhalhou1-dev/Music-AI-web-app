608ce47b402a7678d55f374b94dc36fb33ef3d52
Zhalhou1-dev <83404810+Zhalhou1-dev@users.noreply.github.com>
2021-09-30 12:33:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+songplay=null;
+leftwristx=0;
+leftwristy=0;
+rightWrist=0;
+rightwristy=0;
+function preload(){
+    songplay=loadSound("Aimusic.mp3");
+}
+
+function setup(){
+canvas=createCanvas(400,400);
+canvas.center();
+video=createCapture(VIDEO);
+video.hide();
+
+posenet=ml5.poseNet(video,modelLoaded);
+posenet.on('pose',gotPoses);
+
+}
+function draw(){
+    image(video,0,0,400,400);
+}
+function play(){
+    songplay.play();
+    songplay.rate(1);
+    songplay.setVolume(1);
+}
+function gotPoses(results){
+    if (results.length>0){
+    console.log(results);
+    leftwristx=results[0].pose.leftWrist.x;
+    leftwristy=results[0].pose.leftWrist.y;
+    rightwristx=results[0].pose.rightWrist.x;
+    rightwristy=results[0].pose.rightWrist.y;
+    
+    console.log('Left Wrist x='+leftwristx+'Left Wrist Y='+leftwristy);
+    console.log('Right Wrist x='+rightwristx+'Right Wrist Y='+rightwristy);
+    
+    }
+    }
